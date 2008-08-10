d3b5e7afeafe2f054d6e6d98bbc53fa8dc4ba4d3
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2008-08-10 16:55:33 +0000

    git-svn-id: https://servisto.de/svn/share/trunk/scrum@1731 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/client/ScrumGwtApplication.java
+++ src/main/java/scrum/client/ScrumGwtApplication.java
@@ -14,47 +14,49 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class ScrumGwtApplication implements EntryPoint {
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
-    Image img = new Image("http://code.google.com/webtoolkit/logo-185x175.png");
-    Button button = new Button("Click me");
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		Image img = new Image("http://code.google.com/webtoolkit/logo-185x175.png");
+		Button button = new Button("Start the Ham & Eggs Scrum tool");
 
-    VerticalPanel vPanel = new VerticalPanel();
-    // We can add style names.
-    vPanel.addStyleName("widePanel");
-    vPanel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
-    vPanel.add(img);
-    vPanel.add(button);
+		VerticalPanel vPanel = new VerticalPanel();
+		// We can add style names.
+		vPanel.addStyleName("widePanel");
+		vPanel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
+		vPanel.add(img);
+		vPanel.add(button);
 
-    // Add image and button to the RootPanel
-    RootPanel.get().add(vPanel);
+		// Add image and button to the RootPanel
+		RootPanel.get().add(vPanel);
 
-    // Create the dialog box
-    final DialogBox dialogBox = new DialogBox();
-    dialogBox.setText("Welcome to GWT!");
-    dialogBox.setAnimationEnabled(true);
-    Button closeButton = new Button("close");
-    VerticalPanel dialogVPanel = new VerticalPanel();
-    dialogVPanel.setWidth("100%");
-    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
-    dialogVPanel.add(closeButton);
+		// Create the dialog box
+		final DialogBox dialogBox = new DialogBox();
+		dialogBox.setText("You got so far. Probably you're pig!");
+		dialogBox.setAnimationEnabled(true);
+		Button closeButton = new Button("close");
+		VerticalPanel dialogVPanel = new VerticalPanel();
+		dialogVPanel.setWidth("100%");
+		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
+		dialogVPanel.add(closeButton);
 
-    closeButton.addClickListener(new ClickListener() {
-      public void onClick(Widget sender) {
-        dialogBox.hide();
-      }
-    });
+		closeButton.addClickListener(new ClickListener() {
 
-    // Set the contents of the Widget
-    dialogBox.setWidget(dialogVPanel);
-    
-    button.addClickListener(new ClickListener() {
-      public void onClick(Widget sender) {
-        dialogBox.center();
-        dialogBox.show();
-      }
-    });
-  }
+			public void onClick(Widget sender) {
+				dialogBox.hide();
+			}
+		});
+
+		// Set the contents of the Widget
+		dialogBox.setWidget(dialogVPanel);
+
+		button.addClickListener(new ClickListener() {
+
+			public void onClick(Widget sender) {
+				dialogBox.center();
+				dialogBox.show();
+			}
+		});
+	}
 }
