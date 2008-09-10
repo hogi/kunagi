030aa9d48fb425c26fe00fb9cd7e7df3fc9ad1e3
ako <ako@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2008-09-10 20:45:16 +0000

    TestWidget: weitere DND-Experimente
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@1786 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/gwt/java/scrum/client/test/TestWidget.java
+++ src/gwt/java/scrum/client/test/TestWidget.java
@@ -1,25 +1,91 @@
 package scrum.client.test;
 
+import com.allen_sauer.gwt.dnd.client.DragContext;
+import com.allen_sauer.gwt.dnd.client.PickupDragController;
+import com.allen_sauer.gwt.dnd.client.drop.SimpleDropController;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.MouseListener;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SourcesMouseEvents;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class TestWidget extends Composite {
+
+	private PickupDragController dragController = new PickupDragController(RootPanel.get(), false);
 
 	public TestWidget() {
 		VerticalPanel panel = new VerticalPanel();
 		panel.setStyleName("TestWidget");
-		DragWidget d = new DragWidget();
-		panel.add(d);
+		panel.setSpacing(30);
+
+		DragWidget drag = new DragWidget();
+		DropWidget drop = new DropWidget();
+		dragController.makeDraggable(drag);
+		TestDropController dropController = new TestDropController(drop);
+		dragController.registerDropController(dropController);
+
+		panel.add(drag);
+		panel.add(drop);
 		initWidget(panel);
 	}
 
 	class DragWidget extends Composite implements SourcesMouseEvents {
 
-		public void addMouseListener(MouseListener listener) {}
+		Label label = new Label("dragMe");
 
-		public void removeMouseListener(MouseListener listener) {}
+		public DragWidget() {
+			initWidget(label);
+		}
 
+		public void addMouseListener(MouseListener listener) {
+			label.addMouseListener(listener);
+		}
+
+		public void removeMouseListener(MouseListener listener) {
+			label.removeMouseListener(listener);
+		}
+
+	}
+
+	class DropWidget extends Composite {
+
+		int dropped = 0;
+		Label label = new Label("Drop here! (dropped 0)");
+
+		public DropWidget() {
+			initWidget(label);
+		}
+
+		public void increment() {
+			label.setText("Drop here! (dropped " + (++dropped) + ")");
+		}
+	}
+
+	public class TestDropController extends SimpleDropController {
+
+		private DropWidget widget;
+
+		public TestDropController(Widget dropTarget) {
+			super(dropTarget);
+			widget = (DropWidget) dropTarget;
+		}
+
+		@Override
+		public void onDrop(DragContext context) {
+			super.onDrop(context);
+			widget.increment();
+		}
+
+		@Override
+		public void onEnter(DragContext context) {
+			super.onEnter(context);
+		}
+
+		@Override
+		public void onLeave(DragContext context) {
+			super.onLeave(context);
+		}
 	}
 }
