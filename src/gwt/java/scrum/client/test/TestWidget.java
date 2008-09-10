56ddc27ceb4420859cfda3b8cccf6f16de9f496c
ako <ako@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2008-09-10 21:20:25 +0000

    TestWidget: funktionierendes DND-Beispiel
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@1787 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/gwt/java/scrum/client/test/TestWidget.java
+++ src/gwt/java/scrum/client/test/TestWidget.java
@@ -1,91 +1,29 @@
 package scrum.client.test;
 
-import com.allen_sauer.gwt.dnd.client.DragContext;
 import com.allen_sauer.gwt.dnd.client.PickupDragController;
-import com.allen_sauer.gwt.dnd.client.drop.SimpleDropController;
+import com.allen_sauer.gwt.dnd.client.drop.AbsolutePositionDropController;
+import com.allen_sauer.gwt.dnd.client.drop.DropController;
+import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.MouseListener;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.SourcesMouseEvents;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 public class TestWidget extends Composite {
 
-	private PickupDragController dragController = new PickupDragController(RootPanel.get(), false);
-
 	public TestWidget() {
-		VerticalPanel panel = new VerticalPanel();
-		panel.setStyleName("TestWidget");
-		panel.setSpacing(30);
+		AbsolutePanel dndPanel = new AbsolutePanel();
+		dndPanel.setPixelSize(400, 400);
 
-		DragWidget drag = new DragWidget();
-		DropWidget drop = new DropWidget();
-		dragController.makeDraggable(drag);
-		TestDropController dropController = new TestDropController(drop);
+		PickupDragController dragController = new PickupDragController(RootPanel.get(), true);
+		dragController.setBehaviorConstrainedToBoundaryPanel(false);
+		dragController.setBehaviorMultipleSelection(true);
+		DropController dropController = new AbsolutePositionDropController(dndPanel);
 		dragController.registerDropController(dropController);
 
-		panel.add(drag);
-		panel.add(drop);
-		initWidget(panel);
-	}
+		Label drag = new Label("drag");
+		dragController.makeDraggable(drag);
 
-	class DragWidget extends Composite implements SourcesMouseEvents {
-
-		Label label = new Label("dragMe");
-
-		public DragWidget() {
-			initWidget(label);
-		}
-
-		public void addMouseListener(MouseListener listener) {
-			label.addMouseListener(listener);
-		}
-
-		public void removeMouseListener(MouseListener listener) {
-			label.removeMouseListener(listener);
-		}
-
-	}
-
-	class DropWidget extends Composite {
-
-		int dropped = 0;
-		Label label = new Label("Drop here! (dropped 0)");
-
-		public DropWidget() {
-			initWidget(label);
-		}
-
-		public void increment() {
-			label.setText("Drop here! (dropped " + (++dropped) + ")");
-		}
-	}
-
-	public class TestDropController extends SimpleDropController {
-
-		private DropWidget widget;
-
-		public TestDropController(Widget dropTarget) {
-			super(dropTarget);
-			widget = (DropWidget) dropTarget;
-		}
-
-		@Override
-		public void onDrop(DragContext context) {
-			super.onDrop(context);
-			widget.increment();
-		}
-
-		@Override
-		public void onEnter(DragContext context) {
-			super.onEnter(context);
-		}
-
-		@Override
-		public void onLeave(DragContext context) {
-			super.onLeave(context);
-		}
+		dndPanel.add(drag);
+		initWidget(dndPanel);
 	}
 }
