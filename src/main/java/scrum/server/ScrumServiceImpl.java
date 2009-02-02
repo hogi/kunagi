5ab67e2ed3735f521f1b20a074cf8ddef140f3b8
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2009-02-02 12:01:45 +0000

    logging
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@2061 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/server/ScrumServiceImpl.java
+++ src/main/java/scrum/server/ScrumServiceImpl.java
@@ -1,10 +1,21 @@
 package scrum.server;
 
+import ilarkesto.logging.Logger;
+
+import javax.servlet.ServletException;
+
 public class ScrumServiceImpl extends GScrumServiceImpl {
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		Logger.setDebugEnabled(true);
+	}
 
 	@Override
 	public void destroy() {
 		ScrumWebApplication.get().shutdown();
+		super.destroy();
 	}
 
 }
