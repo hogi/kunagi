b226120e271184896f64c1116c8a2192e6b28c8f
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2009-04-06 19:07:13 +0000

    Bugfix
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@2301 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/client/DataTransferObject.java
+++ src/main/java/scrum/client/DataTransferObject.java
@@ -4,6 +4,8 @@ import ilarkesto.gwt.client.ADataTransferObject;
 
 import java.io.Serializable;
 
-public class DataTransferObject extends ADataTransferObject implements Serializable {
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class DataTransferObject extends ADataTransferObject implements Serializable, IsSerializable {
 
 }
