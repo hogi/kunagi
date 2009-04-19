45a6ca6b493365ed7d87968011843e4466413a3b
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2009-04-19 14:21:58 +0000

    Users Tasks in Sprint
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@2327 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/client/sprint/Sprint.java
+++ src/main/java/scrum/client/sprint/Sprint.java
@@ -1,8 +1,10 @@
 package scrum.client.sprint;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import scrum.client.admin.User;
 import scrum.client.project.Project;
 import scrum.client.project.Requirement;
 
@@ -19,6 +21,24 @@ public class Sprint extends GSprint {
 
 	public List<Requirement> getRequirements() {
 		return getDao().getRequirementsBySprint(this);
+	}
+
+	public List<Task> getTasks(User user) {
+		List<Task> ret = new ArrayList<Task>();
+		for (Requirement requirement : getRequirements()) {
+			for (Task task : requirement.getTasks()) {
+				if (user == null) {
+					if (!task.isOwnerSet()) {
+						ret.add(task);
+					}
+				} else {
+					if (task.isOwner(user)) {
+						ret.add(task);
+					}
+				}
+			}
+		}
+		return ret;
 	}
 
 	public Integer getTaskEffortSum() {
