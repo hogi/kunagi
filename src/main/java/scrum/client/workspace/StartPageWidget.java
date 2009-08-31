4e13ca5d82dad4649a336352936205bdd331073f
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2009-08-31 15:37:39 +0000

    StartPageWidget
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@2432 15bc23fa-3125-0410-a1f3-bdc40dbf1900

---
+++ src/main/java/scrum/client/workspace/StartPageWidget.java
@@ -0,0 +1,43 @@
+package scrum.client.workspace;
+
+import ilarkesto.gwt.client.AWidget;
+import scrum.client.ScrumGwtApplication;
+import scrum.client.admin.ProjectSelectorWidget;
+import scrum.client.admin.UserListWidget;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class StartPageWidget extends AWidget {
+
+	private ProjectSelectorWidget projectSelector;
+	private UserListWidget userList;
+
+	@Override
+	protected Widget onInitialization() {
+		VerticalPanel panel = new VerticalPanel();
+		panel.setWidth("100%");
+		panel.add(getProjectSelector());
+		if (ScrumGwtApplication.get().getUser().isAdmin()) {
+			panel.add(getUserList());
+		}
+		return panel;
+	}
+
+	@Override
+	protected void onUpdate() {
+		if (projectSelector != null) projectSelector.update();
+		if (userList != null) userList.update();
+	}
+
+	public ProjectSelectorWidget getProjectSelector() {
+		if (projectSelector == null) projectSelector = new ProjectSelectorWidget();
+		return projectSelector;
+	}
+
+	public UserListWidget getUserList() {
+		if (userList == null) userList = new UserListWidget();
+		return userList;
+	}
+
+}
