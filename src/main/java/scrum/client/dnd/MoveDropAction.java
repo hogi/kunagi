234f4b91ab540b9bb89cff7808ddd0d2f32f3688
fha <fha@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2009-09-17 20:44:30 +0000

    git-svn-id: https://servisto.de/svn/share/trunk/scrum@2560 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/client/dnd/MoveDropAction.java
+++ src/main/java/scrum/client/dnd/MoveDropAction.java
@@ -1,17 +1,29 @@
 package scrum.client.dnd;
 
 import scrum.client.animation.AppearAnimation;
+import scrum.client.animation.CompletionListener;
+import scrum.client.animation.DisappearAnimation;
+import scrum.client.animation.ObservableAnimation;
 import scrum.client.common.ABlockWidget;
 import scrum.client.common.BlockListWidget;
 
 public class MoveDropAction<T> implements BlockListDropAction<T> {
 
-	public boolean execute(ABlockWidget block, BlockListWidget fromList, int fromIndex, BlockListWidget toList,
-			int toIndex) {
+	public boolean execute(final ABlockWidget block, final BlockListWidget fromList, final int fromIndex,
+			final BlockListWidget toList, final int toIndex) {
 		if (fromList != toList) return false;
-		AppearAnimation ani = new AppearAnimation(block, block.getOffsetHeight());
-		toList.move(block, toIndex);
-		ani.run(400);
+		final int height = block.getOffsetHeight();
+		DisappearAnimation aniDisappear = new DisappearAnimation(block, height);
+		aniDisappear.addCompletionListener(new CompletionListener() {
+
+			public void completionEvent(ObservableAnimation source) {
+				AppearAnimation aniAppear = new AppearAnimation(block, height);
+				toList.move(block, toIndex);
+				aniAppear.run(120);
+			}
+		});
+		aniDisappear.run(120);
+
 		return true;
 	}
 }
