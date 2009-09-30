ea20ce5cc7197bb0288c634f3a3b8b0c454bdad5
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2009-09-30 20:53:06 +0000

    widgets tester on login page
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@2628 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/client/context/PublicContext.java
+++ src/main/java/scrum/client/context/PublicContext.java
@@ -1,11 +1,12 @@
 package scrum.client.context;
 
 import ilarkesto.gwt.client.SwitchingNavigatorWidget;
+import scrum.client.ApplicationInfo;
+import scrum.client.ScrumGwtApplication;
 import scrum.client.admin.LoginWidget;
 import scrum.client.test.WidgetsTesterWidget;
 import scrum.client.workspace.Ui;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
@@ -21,7 +22,10 @@ public class PublicContext extends AContext {
 		navigator.addItem((Image) null, "Login", login);
 		navigator.addItem((Image) null, "Register", new Label(""));
 		navigator.addItem((Image) null, "About", new Label(""));
-		if (GWT.isClient()) navigator.addItem((Image) null, "Widgets Tests", new WidgetsTesterWidget());
+		ApplicationInfo applicationInfo = ScrumGwtApplication.get().getApplicationInfo();
+		if (applicationInfo != null && applicationInfo.isDevelopmentStage()) {
+			navigator.addItem((Image) null, "Widgets Tests", new WidgetsTesterWidget());
+		}
 		return navigator;
 	}
 
