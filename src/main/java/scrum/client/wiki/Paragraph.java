2c726bbb84eeb45c42b7d59f2144a88677e6b2e0
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2009-11-04 22:30:50 +0000

    wiki
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@2899 15bc23fa-3125-0410-a1f3-bdc40dbf1900

---
+++ src/main/java/scrum/client/wiki/Paragraph.java
@@ -0,0 +1,38 @@
+package scrum.client.wiki;
+
+import ilarkesto.gwt.client.Gwt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Paragraph extends AWikiElement {
+
+	private List<AWikiElement> items = new ArrayList<AWikiElement>();
+	private boolean p;
+
+	public Paragraph(boolean p) {
+		super();
+		this.p = p;
+	}
+
+	public void add(AWikiElement item) {
+		items.add(item);
+	}
+
+	@Override
+	String toHtml() {
+		StringBuilder sb = new StringBuilder();
+		if (p) sb.append("<p>");
+		for (AWikiElement item : items) {
+			sb.append(item.toHtml());
+		}
+		if (p) sb.append("</p>");
+		return sb.toString();
+	}
+
+	@Override
+	public String toString() {
+		return "Paragraph(" + Gwt.toString(items) + ")";
+	}
+
+}
