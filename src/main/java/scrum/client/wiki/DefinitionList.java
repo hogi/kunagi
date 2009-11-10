9147121b765d81a85aa5d92c40f04823e459a4d9
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2009-11-10 17:19:06 +0000

    wiki parser
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@2943 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/client/wiki/DefinitionList.java
+++ src/main/java/scrum/client/wiki/DefinitionList.java
@@ -5,15 +5,9 @@ import ilarkesto.gwt.client.Gwt;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ItemList extends AWikiElement {
+public class DefinitionList extends AWikiElement {
 
 	private List<Paragraph> items = new ArrayList<Paragraph>();
-	boolean ordered;
-
-	public ItemList(boolean ordered) {
-		super();
-		this.ordered = ordered;
-	}
 
 	public void add(Paragraph item) {
 		items.add(item);
@@ -22,19 +16,19 @@ public class ItemList extends AWikiElement {
 	@Override
 	String toHtml() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(ordered ? "<ol>" : "<ul>");
+		sb.append("<dl>");
 		for (Paragraph item : items) {
 			sb.append("<li>");
 			sb.append(item.toHtml());
 			sb.append("</li>");
 		}
-		sb.append(ordered ? "</ol>" : "</ul>");
+		sb.append("</dl>");
 		return sb.toString();
 	}
 
 	@Override
 	public String toString() {
-		return "ItemList(" + Gwt.toString(items) + ")";
+		return "DefinitionList(" + Gwt.toString(items) + ")";
 	}
 
 }
