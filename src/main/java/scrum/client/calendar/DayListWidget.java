26dce749ef86086b743cfe0f2cbff4ff030bc8a7
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2009-11-18 20:14:31 +0000

    calendar
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@2994 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/client/calendar/DayListWidget.java
+++ src/main/java/scrum/client/calendar/DayListWidget.java
@@ -12,7 +12,6 @@ import scrum.client.common.BlockListSelectionManager;
 import scrum.client.common.BlockListWidget;
 
 import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -50,20 +49,26 @@ public class DayListWidget extends AScrumWidget {
 		int row = 0;
 		Date date = dateToShow;
 		Date end = dateToShow.addDays(14);
+		int month = 0;
 		int week = 0;
 		while (date.compareTo(end) <= 0) {
+			int m = date.getMonth();
+			if (m != month) {
+				month = m;
+				table.setWidget(row, 0, Gwt.createDiv("DayListWidget-month", Gwt.getMonthShort(month)));
+			}
 			int w = date.getWeek();
 			if (w != week) {
 				week = w;
-				table.setWidget(row, 0, createWeek(week));
-				// table.getCellFormatter().getElement(row, 0).setAttribute("rowspan", "7");
+				table.setWidget(row, 1, Gwt.createDiv("DayListWidget-week", String.valueOf(week)));
 			} else {
-				table.setWidget(row, 0, new Label("."));
+				// table.setWidget(row, 0, new Label("."));
+				// table.setWidget(row, 0, Gwt.createDiv("DayListWidget-week", String.valueOf(week)));
 			}
-			table.setWidget(row, 1, Gwt
-					.createDiv("DayListWidget-date", Gwt.DTF_WEEKDAY_SHORT.format(date.toJavaDate())));
 			table.setWidget(row, 2, Gwt.createDiv("DayListWidget-date", Gwt.DTF_DAY.format(date.toJavaDate())));
-			table.setWidget(row, 3, createEventList(date));
+			table.setWidget(row, 3, Gwt
+					.createDiv("DayListWidget-date", Gwt.DTF_WEEKDAY_SHORT.format(date.toJavaDate())));
+			table.setWidget(row, 4, createEventList(date));
 			date = date.nextDay();
 			row++;
 		}
@@ -75,12 +80,6 @@ public class DayListWidget extends AScrumWidget {
 		showDate(event.getDate());
 		update();
 		selectionManager.select(event);
-	}
-
-	private Widget createWeek(int week) {
-		Label l = new Label("" + week);
-		l.setStyleName("DayListWidget-week");
-		return l;
 	}
 
 	private Widget createEventList(Date date) {
