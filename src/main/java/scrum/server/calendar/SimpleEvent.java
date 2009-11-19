93306df9971e73786bac5088a10687b804ca0bc3
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2009-11-19 16:54:41 +0000

    bugfix
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@3012 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/server/calendar/SimpleEvent.java
+++ src/main/java/scrum/server/calendar/SimpleEvent.java
@@ -1,11 +1,18 @@
 package scrum.server.calendar;
 
+import ilarkesto.base.time.Date;
 import scrum.server.admin.User;
 
 public class SimpleEvent extends GSimpleEvent {
 
 	public boolean isVisibleFor(User user) {
 		return getProject().isVisibleFor(user);
+	}
+
+	@Override
+	public void ensureIntegrity() {
+		if (!isDateSet()) setDate(Date.today());
+		super.ensureIntegrity();
 	}
 
 }
