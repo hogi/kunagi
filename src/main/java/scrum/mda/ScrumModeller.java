9b81eee5ed7e4b15d96dc017f5b43aa600406b7a
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2010-02-26 14:59:32 +0000

    new mda modeller integrated
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@3359 15bc23fa-3125-0410-a1f3-bdc40dbf1900

---
+++ src/main/java/scrum/mda/ScrumModeller.java
@@ -0,0 +1,24 @@
+package scrum.mda;
+
+import ilarkesto.core.scope.Scope;
+import ilarkesto.mda.model.CsvFileModelSource;
+import ilarkesto.mda.model.ModellingSession;
+import ilarkesto.mda.swingeditor.Starter;
+import ilarkesto.mda.swingeditor.Workspace;
+
+import java.io.File;
+
+public class ScrumModeller extends Starter {
+
+	public static void main(String[] args) {
+		createModellerScope();
+
+		Scope scope = Scope.get();
+
+		ModellingSession modellingSession = scope.getComponent(ModellingSession.class);
+		modellingSession.load(new CsvFileModelSource(new File("src/model.csv")));
+
+		scope.getComponent(Workspace.class).showJFrame();
+	}
+
+}
