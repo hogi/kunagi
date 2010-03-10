b9972276783d9e8dd99b953469d6e0a642713005
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2010-03-10 21:22:23 +0000

    refactoring
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@3413 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/client/ScrumComponentsReflector.java
+++ src/main/java/scrum/client/ScrumComponentsReflector.java
@@ -1,12 +1,6 @@
 package scrum.client;
 
-import scrum.client.communication.PingerReflector;
 
 public class ScrumComponentsReflector extends GScrumComponentsReflector {
-
-	@Override
-	public PingerReflector createPingerReflector() {
-		return new PingerReflector();
-	}
 
 }
