4506bc9adf3bd2a6dfef3237f9f11275660f550c
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2010-06-24 17:13:11 +0000

    mda legacy processor in ScrumModeller
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@3714 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/mda/ScrumModeller.java
+++ src/main/java/scrum/mda/ScrumModeller.java
@@ -1,7 +1,10 @@
 package scrum.mda;
 
 import ilarkesto.core.scope.Scope;
+import ilarkesto.di.app.ApplicationStarter;
 import ilarkesto.mda.model.CsvFileModelSource;
+import ilarkesto.mda.model.Model;
+import ilarkesto.mda.model.ModelProcessor;
 import ilarkesto.mda.model.ModellingSession;
 import ilarkesto.mda.swingeditor.Starter;
 import ilarkesto.mda.swingeditor.Workspace;
@@ -14,9 +17,18 @@ public class ScrumModeller extends Starter {
 		Scope scope = createModellerScope();
 
 		ModellingSession modellingSession = scope.getComponent(ModellingSession.class);
+		modellingSession.addProcessor(new LegacyGeneration());
 		modellingSession.load(new CsvFileModelSource(new File("src/model.csv")));
 
 		scope.getComponent(Workspace.class).showJFrame();
+	}
+
+	static class LegacyGeneration implements ModelProcessor {
+
+		public void processModel(Model model) {
+			ApplicationStarter.startApplication(ScrumModelApplication.class).generateClasses().shutdown();
+		}
+
 	}
 
 }
