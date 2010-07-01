2b7a14882d291d03e880c75a020435566dc0a117
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2010-07-01 17:21:12 +0000

    lock screen while password request
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@3746 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/client/admin/RequestNewPasswordAction.java
+++ src/main/java/scrum/client/admin/RequestNewPasswordAction.java
@@ -3,6 +3,7 @@ package scrum.client.admin;
 import ilarkesto.core.base.Str;
 import ilarkesto.core.scope.Scope;
 import scrum.client.communication.ServerErrorManager;
+import scrum.client.workspace.Ui;
 
 public class RequestNewPasswordAction extends GRequestNewPasswordAction {
 
@@ -24,14 +25,15 @@ public class RequestNewPasswordAction extends GRequestNewPasswordAction {
 			loginWidget.setFailed("Email required.");
 			return;
 		}
+		Scope.get().getComponent(Ui.class).getWorkspace().lock("Process registration...");
 		new RequestNewPasswordServiceCall(login).execute(new Runnable() {
 
 			public void run() {
 				String message = Scope.get().getComponent(ServerErrorManager.class).popErrorsAsString();
 				if (message == null) message = "Password will be sent to you by email.";
 				loginWidget.setFailed(message);
+				Scope.get().getComponent(Ui.class).getWorkspace().unlock();
 			}
 		});
 	}
-
 }
