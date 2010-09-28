7619cbc7bd478185239a189e5089cc204d98c999
wko <wko@15bc23fa-3125-0410-a1f3-bdc40dbf1900>
2010-09-28 18:05:20 +0000

    bugfix
    git-svn-id: https://servisto.de/svn/share/trunk/scrum@4093 15bc23fa-3125-0410-a1f3-bdc40dbf1900

--- src/main/java/scrum/client/project/CreateExampleProjectAction.java
+++ src/main/java/scrum/client/project/CreateExampleProjectAction.java
@@ -4,6 +4,7 @@ import ilarkesto.core.scope.Scope;
 
 import java.util.List;
 
+import scrum.client.common.TooltipBuilder;
 import scrum.client.workspace.Ui;
 import scrum.client.workspace.UsersWorkspaceWidgets;
 
@@ -15,11 +16,26 @@ public class CreateExampleProjectAction extends GCreateExampleProjectAction {
 	}
 
 	@Override
+	public String getTooltip() {
+		TooltipBuilder tb = new TooltipBuilder("Create a new example Project with some content.");
+		if (!getCurrentUser().isAdmin() && getDao().getSystemConfig().isProjectCreationDisabled())
+			tb.addRemark("Creating new projects is disabled.");
+		return tb.getTooltip();
+	}
+
+	@Override
+	public boolean isPermitted() {
+		if (!getCurrentUser().isAdmin() && getDao().getSystemConfig().isProjectCreationDisabled()) return false;
+		return true;
+	}
+
+	@Override
 	protected void onExecute() {
 		Scope.get().getComponent(Ui.class).lock("Creating Example Project...");
 		final List<Project> previousProjects = getDao().getProjects();
 		new CreateExampleProjectServiceCall().execute(new Runnable() {
 
+			@Override
 			public void run() {
 				Scope.get().getComponent(Ui.class).unlock();
 				List<Project> newProjects = getDao().getProjects();
